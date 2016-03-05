cf439b2af0382e5f70c01972da814d0037ad75a5
10000highfives <jared.a.fowler@gmail.com>
2016-03-04 19:31:54 -0800

    adds tests for htmlToString

--- test/test.js
+++ test/test.js
@@ -1,10 +1,22 @@
 /* global describe, it */
+'use strict'
 const assert = require('assert')
-const makeArrayOfContents = require('../makeArrayOfContents')
+const path = require('path')
+const makeArrayOfContents = require('../lib/makeArrayOfContents')
+const htmlToString = require('../lib/htmlToString')
+
+const testHtml = '<!DOCTYPE><html><body><script src="https://cdn.jsdelivr.net/webtorrent/latest/webtorrent.min.js"></script></body></html>'
 
 // makeArrayOfContents
 describe('makeArrayOfContents', function () {
   it('should return array of files in directory', function () {
-    assert.equal(['fun.jpg', 'laughter.html', 'neat.png'], makeArrayOfContents('test/test-dir'))
+    assert.equal(['fun.jpg', 'laughter.html', 'neat.png'],
+    makeArrayOfContents(path.join(__dirname + '/test-dir')))
+  })
+})
+
+describe('htmlToString', function (done) {
+  it('should read a file and return a string of the file contents', function () {
+    assert.equal(testHtml, htmlToString(path.join(__dirname, '/replaceHtml/index.html')))
   })
 })
