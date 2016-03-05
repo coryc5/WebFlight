f2796ea6d398eb74f709de18e26b1f8bd46fc405
10000highfives <jared.a.fowler@gmail.com>
2016-03-04 17:16:24 -0800

    adds test for readdir

--- test/test.js
+++ test/test.js
@@ -1,4 +1,10 @@
 /* global describe, it */
-var assert = require('assert')
+const assert = require('assert')
+const makeArrayOfContents = require('../makeArrayOfContents')
 
-//	Write tests here!
+// makeArrayOfContents
+describe('makeArrayOfContents', function () {
+  it('should return array of files in directory', function () {
+    assert.equal(['fun.jpg', 'laughter.html', 'neat.png'], makeArrayOfContents('test/test-dir'))
+  })
+})
