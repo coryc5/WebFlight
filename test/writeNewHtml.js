c3d816f009b81291a93d3d3c8d976c37e537d67a
Cory Caprice <corycaprice@gmail.com>
2016-03-04 16:58:40 -0800

    Test for write new html

--- test/writeNewHtml.js
+++ test/writeNewHtml.js
@@ -13,17 +13,13 @@ describe('writeNewHtml', () => {
 
     fs.mkdir(__dirname + '/wf', err => {
       writeNewHtml(html, 'test/wf/index');
-      
-      setTimeout(() => {
-        var wroteHtml = fs.statSync(__dirname + '/wt/index.html').isFile()
-        fs.unlink(__dirname + '/wf/index.html', err => {
-          console.log('unlinking...')
-          fs.rmdir(__dirname + '/wf', err => {
-            assert.equal(true, wroteHtml)
-            done()
-          })
+      var wroteHtml = fs.statSync(__dirname + '/wf/index.html').isFile()
+      fs.unlink(__dirname + '/wf/index.html', err => {
+        fs.rmdir(__dirname + '/wf', err => {
+          assert.equal(true, wroteHtml)
+          done()
         })
-      }, 1500)
+      })
     })
   })
 })
