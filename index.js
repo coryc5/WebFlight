8161ea1f294a8d20f8c8ad5f91c21e385be6ad4e
Cory Caprice <corycaprice@gmail.com>
2016-03-14 14:21:24 -0700

    Refactor to comply with JS standard rules

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const hashFilesObj = require('./lib/hashFilesObj')
 const writeJs = require('./lib/writeJs')
 const replaceHtml = require('./lib/replaceHtml')
 const writeNewHtml = require('./lib/writeNewHtml')
+const botGenerator = require(('./server/botGenerator'))
+
+const WebFlight = {}
 
 // options :: Object
   // originalHtml: .html file to be rebuilt
@@ -13,16 +16,17 @@ const writeNewHtml = require('./lib/writeNewHtml')
   // route: path on the server for files
   // jsOutput: location and name for webflight.js file
   // htmlOutput: location and name for rebuilt html file
-function WebFlight(options) {
+WebFlight.start = (options) => {
   return new Promise((resolve, reject) => {
     const originalHtmlString = stringifyHtml(options.originalHtml)
     const filesObj = makeFilesObj(options.filesFolder, options.route)
-    
+
     hashFilesObj(filesObj)
       .then(writeJs.bind(null, options.jsOutput))
       .then(replaceHtml.bind(null, originalHtmlString))
       .then(writeNewHtml.bind(null, options.htmlOutput))
-      .then(resolve)
+      // call botGenerator
+      .then(resolve(options))
   })
 }
 
