73b7a9286de7432c7891c26f27a046d0c7bb2490
Cory Caprice <corycaprice@gmail.com>
2016-03-16 11:56:15 -0700

    Prep index.js for new config obj

--- src/botGenerator.js
+++ src/botGenerator.js
@@ -7,9 +7,9 @@ function botGenerator (options) {
   const electron = spawn(options.jsOutputUL, {
     detached: true
   })
-  electron.stderr.on('data', function (data) {
-    console.error('error', data.toString())
-  })
+  // electron.stderr.on('data', function (data) {
+  //   console.error('error', data.toString())
+  // })
   electron.stdout.on('data', function (data) {
     console.log(data.toString())
   })
