7347f5cf4de8c76bd55f86e6d0045a2382692cfe
tbywong <tbywong@gmail.com>
2016-03-17 11:58:55 -0700

    updated constructor function with userCount input and error logs

--- index.js
+++ index.js
@@ -16,15 +16,18 @@ function WebFlight (options) {
     this[key] = options[key]
   })
 
-  let fileNamesArr = Object.keys(this.routes).map((file) => {
-    return path.basename(file, '.html')
-  })
+  let fileNamesArr = (() => {
+    let fileArray = []
+    for (let route in this.routes) {
+      fileArray.push(path.basename(this.routes[route], '.html'))
+    }
+    return fileArray
+  })()
 
   this.count = 0  // non-configurable
-  this.fileNames = fileNamesArr // non-configurable
-
-  this.wfPath = options.wfPath ? 'options.wfPath' : (__dirname + '/wfPath')  // default
-  this.wfRoute = options.wfRoute ? 'options.wfRoute' : ('/wfRoute')  // default
+  this.fileNames = fileNamesArr  // non-configurable
+  this.wfPath = options.wfPath ? options.wfPath : (__dirname + '/wfPath')  // default
+  this.wfRoute = options.wfRoute ? options.wfRoute : ('/wfRoute')  // default
 
   this.seedScript = options.seedScript  // default
   ? options.seedScript
@@ -41,25 +44,35 @@ function WebFlight (options) {
       return `${this.wfPath}/wf-${file}.html`
     })
   })()
+
+  this.userCount = options.userCount ? options.userCount : 10  // default (redirect)
+  this.prepCount = Math.floor(this.userCount * 0.75)  // non-configurable (start bots)
+  this.stopCount = Math.floor(this.userCount * 0.50)  // non-configurable (kill bots, redirect back)
+
+  if (!this.siteUrl) console.error('Error: WebFlight options object requires "siteUrl" property')
+  if (!this.assetsPath) console.error('Error: WebFlight options object requires "assetsPath" property')
+  if (!this.assetsRoute) console.error('Error: WebFlight options object requires "assetsRoute" property')
+  if (!this.routes) console.error('Error: WebFlight options object requires "routes" property')
 }
 
 // options :: Object
-  // siteUrl: String                (required)
+  // siteUrl: String            (required)
   // assetsPath: String|Array   (required)
   // assetsRoute: String|Array  (required)
+  // routes: Object             (required)
+  // userCount: Number          (optional - defaults to 10)
   // wfPath: String             (optional - defaults to '/wfPath')
   // wfRoute: String            (optional - defaults to '/wfRoute')
   // seedScript: String         (optional - defaults to 'wf-seed.js')
-  // routes: Object             (required)
 
+  //  siteUrl: ''
   //  assetsPath: ''/['', ''],
   //  assetsRoute: ''/['', ''],
+  //  routes: {'/about.html': 'path/to/about.html'}
+  //  userCount: 10
   //  wfPath: ''/Default(__dirname + '/wfPath'),
   //  wfRoute: ''/Default('/wfRoute'),
   //  seedScript: ''/Default('wf-seed.js'),
-  //  routes: {
-  //    '/about.html': 'path/to/about.html'
-  //  }
 
 WebFlight.prototype.start = function () {
   const originalHtmlString = stringifyHtml(this.originalHtml)
