e721599265e828b81678d535c7d230e34b937d31
10000highfives <jared.a.fowler@gmail.com>
2016-03-16 21:18:58 -0700

    adds tests for new version of makeFilesObj fn

--- test/makeFilesObj.js
+++ test/makeFilesObj.js
@@ -8,11 +8,21 @@ const makeFilesObj = require('../lib/makeFilesObj')
 describe('makeFilesObj', function () {
   it('should return array of files in directory', function () {
     const array = makeFilesObj(path.join(__dirname + '/test-dir'), 'files/')
-    
+
     assert.deepEqual({
-      'files/fun.jpg': { fileOnServer: `${__dirname}/test-dir/fun.jpg`}, 
-      'files/laughter.html': { fileOnServer: `${__dirname}/test-dir/laughter.html`}, 
-      'files/neat.png': { fileOnServer: `${__dirname}/test-dir/neat.png`}
+      'files/fun.jpg': {fileOnServer: `${__dirname}/test-dir/fun.jpg`},
+      'files/laughter.html': {fileOnServer: `${__dirname}/test-dir/laughter.html`},
+      'files/neat.png': {fileOnServer: `${__dirname}/test-dir/neat.png`}
     }, array)
+  })
+  it('should work with array of directories and single route', function () {
+    const filesObj = makeFilesObj([path.join(__dirname, '/wf'), path.join(__dirname, '/replaceHtml')], '/files')
+
+    assert.deepEqual({
+      'files/webtorrent-breaks-if-folder-is-empty': {fileOnServer: `${__dirname}/wf/webtorrent-breaks-if-folder-is-empty`},
+      'files/t.txt': {fileOnServer: `${__dirname}/wf/t.txt`},
+      'files/index.html': {fileOnServer: `${__dirname}/replaceHtml/index.html`},
+      'files/index2.html': {fileOnServer: `${__dirname}/replaceHtml/index2.html`}
+    }, filesObj)
   })
 })
