f6231d380cbe4d06e908a992d9d23fc4cf562a5b
10000highfives <jared.a.fowler@gmail.com>
2016-03-17 13:57:31 -0700

    use path.join to set path to conform to standard

--- server/althtml.js
+++
@@ -1,8 +0,0 @@
-module.exports = althtml;
-
-function althtml(req,res){
-  if (req.url === "/somepath"){
-    //file path to be decided when we create newDir where stringified html will live
-    res.sendFile(__dirname + '/newDir/index.html')
-  }
-}
