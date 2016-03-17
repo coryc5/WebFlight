3cf972b4454675fbce08e82cdfb072d536b1f842
10000highfives <jared.a.fowler@gmail.com>
2016-03-17 11:13:05 -0700

    Adds test for array inputs

--- test/makeFilesObj.js
+++ test/makeFilesObj.js
@@ -25,4 +25,19 @@ describe('makeFilesObj', function () {
       'files/index2.html': {fileOnServer: `${__dirname}/replaceHtml/index2.html`}
     }, filesObj)
   })
+
+  it('should work with array of directories and array of routes', function () {
+    const filesObj = makeFilesObj([path.join(__dirname, '/wf'), path.join(__dirname, '/replaceHtml')], ['/files', '/img'])
+
+    assert.deepEqual({
+      'files/webtorrent-breaks-if-folder-is-empty': {fileOnServer: `${__dirname}/wf/webtorrent-breaks-if-folder-is-empty`},
+      'files/t.txt': {fileOnServer: `${__dirname}/wf/t.txt`},
+      'files/index.html': {fileOnServer: `${__dirname}/replaceHtml/index.html`},
+      'files/index2.html': {fileOnServer: `${__dirname}/replaceHtml/index2.html`},
+      'img/webtorrent-breaks-if-folder-is-empty': {fileOnServer: `${__dirname}/wf/webtorrent-breaks-if-folder-is-empty`},
+      'img/t.txt': {fileOnServer: `${__dirname}/wf/t.txt`},
+      'img/index.html': {fileOnServer: `${__dirname}/replaceHtml/index.html`},
+      'img/index2.html': {fileOnServer: `${__dirname}/replaceHtml/index2.html`}
+    }, filesObj)
+  })
 })
