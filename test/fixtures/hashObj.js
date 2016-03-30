f0ab8ac557dc0e4c664ab38f63466d3b0aecdf98
Cory Caprice <corycaprice@gmail.com>
2016-03-30 14:32:38 -0700

    Remove console logs and update tests

--- test/fixtures/hashObj.js
+++ test/fixtures/hashObj.js
@@ -12,6 +12,6 @@ module.exports = {
         '../imgs/apple.png',
         '../imgs/netflix.png' ],
     hash: 'cd60283e8886468861b5a3e51b67ccc7b1bef317',
-    magnet: 'magnet:?xt=urn:btih:cd60283e8886468861b5a3e51b67ccc7b1bef317&dn=google.png&tr=udp://exodus.desync.com:6969&tr=udp://tracker.coppersurfer.tk:6969&tr=udp://tracker.internetwarriors.net:1337&tr=udp://tracker.leechers-paradise.org:6969&tr=udp://tracker.openbittorrent.com:80&tr=wss://tracker.btorrent.xyz&tr=wss://tracker.openwebtorrent.com&tr=wss://tracker.webtorrent.io'
+    magnet: 'magnet:?xt=urn:btih:cd60283e8886468861b5a3e51b67ccc7b1bef317&dn=google.png&tr=udp://exodus.desync.com:6969&tr=udp://tracker.coppersurfer.tk:6969&tr=udp://tracker.internetwarriors.net:1337&tr=udp://tracker.leechers-paradise.org:6969&tr=udp://tracker.openbittorrent.com:80&tr=wss://tracker.btorrent.xyz&tr=wss://tracker.fastcast.nz&tr=wss://tracker.openwebtorrent.com&tr=wss://tracker.webtorrent.io'
   }
 }
