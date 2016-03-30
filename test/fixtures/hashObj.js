fe8c83b77ac4269ae3e4dac095360f0c86174fbc
10000highfives <jared.a.fowler@gmail.com>
2016-03-29 18:29:17 -0700

    updates opts.js fixture to use path.join & dirname instead of hardcoded paths.

--- test/fixtures/hashObj.js
+++ test/fixtures/hashObj.js
@@ -1,9 +1,11 @@
+const path = require('path')
 module.exports = {
   '0': {
     paths:
-      [ '/Users/corycaprice/Documents/Programming/Codesmith/WebFlight/test/fixtures/imgs/google.png',
-        '/Users/corycaprice/Documents/Programming/Codesmith/WebFlight/test/fixtures/imgs/apple.png',
-        '/Users/corycaprice/Documents/Programming/Codesmith/WebFlight/test/fixtures/imgs/netflix.png' ],
+      [ path.join(__dirname, 'imgs/google.png'),
+        path.join(__dirname, 'imgs/apple.png'),
+        path.join(__dirname, 'imgs/netflix.png'),
+      ],
     srcs:
       [ '../imgs/google.png',
         '../imgs/apple.png',
@@ -12,3 +14,4 @@ module.exports = {
     magnet: 'magnet:?xt=urn:btih:cd60283e8886468861b5a3e51b67ccc7b1bef317&dn=google.png&tr=udp://exodus.desync.com:6969&tr=udp://tracker.coppersurfer.tk:6969&tr=udp://tracker.internetwarriors.net:1337&tr=udp://tracker.leechers-paradise.org:6969&tr=udp://tracker.openbittorrent.com:80&tr=wss://tracker.btorrent.xyz&tr=wss://tracker.openwebtorrent.com&tr=wss://tracker.webtorrent.io'
   }
 }
+
