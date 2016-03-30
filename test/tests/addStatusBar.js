fac56482fe8c310d3b894adf23408cca246e6ff6
tbywong <tbywong@gmail.com>
2016-03-29 17:47:20 -0700

    tests update

--- test/tests/addStatusBar.js
+++ test/tests/addStatusBar.js
@@ -1,4 +1,20 @@
 const chai = require('chai')
 const expect = chai.expect
+const assert = chai.assert
 
-// const 
+const addStatusBar = require('../../lib/addStatusBar')
+
+describe('addStatusBar', () => {
+  it('files widget should be appended to the page', () => {
+
+  })
+  it('widget should have dropdown functionality', () => {
+
+  })
+  it('dropdown should contain correct number of files', () => {
+
+  })
+  it('close button should work', () => {
+
+  })
+})
--- test/tests/writeJsUL.js
+++ test/tests/writeJsUL.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const path = require('path')
 const chai = require('chai')
 const chaifs = require('chai-fs')
@@ -11,14 +13,20 @@ const filesObj = require('../fixtures/filesObj.js')
 // TODO: Delete existing output file before running test
 const output = path.join(__dirname, '../fixtures/wfPath/js/wf-seed.js')
 
-writeJsUL(output, 'http://localhost:3000', 5, filesObj)
-
 describe('writeJsUL', () => {
+  writeJsUL(output, 'http://localhost:3000', 5, filesObj)
   it('output file should be created', () => {
     assert.isFile(output)
   })
   it('file should "client.seed" all files in obj', () => {
-    // expect(output).to.have.content('client.seed')
-    expect(output).to.not.have.content('undefined')
+    expect(output).to.have.content.that.match(/client.seed/)
+    expect(output).to.not.have.content.that.match(/undefined/)
+  })
+  it('file should contain all paths to the seeded content', () => {
+    const files = Object.keys(filesObj)
+    files.forEach((file) => {
+      const regFile = new RegExp(filesObj[file].path, 'g')
+      expect(output).to.have.content.that.match(regFile)
+    })
   })
 })
